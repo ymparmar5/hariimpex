13eeec55fe32b0bd00c3f78acaac82c55b0b7c8b
ynparmar07 <ymparmar7@gmail.com~>
2024-06-28 18:50:38 +0530

    first commit

--- src/Components/BuyNow.jsx
+++ src/Components/BuyNow.jsx
@@ -190,36 +190,191 @@
 
 // export default BuyNow;
 
-import axios from "axios"
+import React, { useState } from 'react';
+import axios from 'axios';
+import { fireDB } from '../FireBase/FireBaseConfig';
+import { Timestamp, addDoc, collection } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { v4 as uuidv4 } from "uuid";
+import { Buffer } from 'buffer';
+import sha256 from "crypto-js/sha256";
 
-const BuyNow = ()=>{
-    let data ={
-        name:"vikas",
-        amount:1,   
-        number:'9999999999',
-        MID: 'MID' + Date.now(),
-        transactionId: 'T' + Date.now(),
+
+
+
+const BuyNow = () => {
+
+
+    const [data, setData] = useState();
+
+    const handleFormData = (e) => {
+        // console.log(e.target.value);
+        const dd = { ...data, [e.target.name]: e.target.value };
+        setData(dd);
+    };
+
+
+    const makePayment = async (e) => {
+
+        e.preventDefault();
+
+        const transactionid = "Tr-" + uuidv4().toString(36).slice(-6);
+
+        const payload = {
+            merchantId: "M22EJDYBLPU2G",
+            merchantTransactionId: transactionid,
+            merchantUserId: 'MUID-' + uuidv4().toString(36).slice(-6),
+            amount: 10000,
+            redirectUrl: `http://localhost:3000/api/status/${transactionid}`,
+            redirectMode: "POST",
+            callbackUrl: `http://localhost:5173/callback${transactionid}`,
+            mobileNumber: '9999999999',
+            paymentInstrument: {
+                type: "PAY_PAGE",
+            },
+        };
+
+
+        const dataPayload = JSON.stringify(payload);
+        console.log(dataPayload);
+
+        const dataBase64 = Buffer.from(dataPayload).toString("base64");
+        console.log(dataBase64);
+
+
+        const fullURL =
+            dataBase64 + "/pg/v1/pay" + "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399"
+
+            ;
+        const dataSha256 = sha256(fullURL);
+
+        const checksum = dataSha256 + "###" + "d7a8e4458caa6fcd781166bbdc85fec76740c18cb9baa9a4c48cf2387d554180###1";
+        console.log("c====", checksum);
+
+
+
+        const UAT_PAY_API_URL =
+            "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
+
+
+        const response = await axios.post(
+            UAT_PAY_API_URL,
+            {
+                request: dataBase64,
+            },
+            {
+                headers: {
+                    accept: "application/json",
+                    "Content-Type": "application/json",
+                    "X-VERIFY": checksum,
+                },
+            }
+        );
+
+
+        const redirect = response.data.data.instrumentResponse.redirectInfo.url;
+        router.push(redirect)
+
 
     }
 
-    const pay =async()=>{
 
-        try {
-            await axios.post("http://localhost:5173/order",data).then(res=>{
-                console.log(res.data)
-            }).catch(err =>{
-                console.log(err)
-            })
-        } catch (error) {
-            console.log(error)
-        }
-
-    }
-    return<>
-    
-    <button  className="login-to-buy-button" onClick={pay} >
-        pay now
-        </button></>
+    return (
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" action="#" method="POST">
+                    <div>
+                        <label
+                            htmlFor="name"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="name"
+                                name="name"
+                                value="DemoTest"
+                                onChange={(e) => handleFormData(e)}
+                                type="name"
+                                autoComplete="name"
+                                required=""
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="Mobile"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Mobile
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="Mobile"
+                                name="mobile"
+                                value="999999999"
+                                onChange={(e) => handleFormData(e)}
+                                autoComplete="Mobile"
+                                required=""
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="Amount"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Amount
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="Amount"
+                                name="amount"
+                                value="10"
+                                autoComplete="Amount"
+                                onChange={(e) => handleFormData(e)}
+                                required=""
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <label
+                            htmlFor="MUID"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            MUID
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="MUID"
+                                name="muid"
+                                value="nuid-909090"
+                                onChange={(e) => handleFormData(e)}
+                                autoComplete="MUID"
+                                required=""
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    <div></div>
+                    <div>
+                        <button
+                            onClick={(e) => makePayment(e)}
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Pay
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
+
 
 export default BuyNow;
